c63f67a83a591ecf3c7b203b99c9e292b8f095b7
Mindaugas Mazūra <mazura.mindaugas@gmail.com>
2023-06-27 10:18:04 +0300

    Bug fix backEnd

--- backEnd/examTask/src/main/java/com/praktikosEgzaminas/examTask/income/IncomeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.praktikosEgzaminas.examTask.income;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IncomeRepository extends JpaRepository<Income, Long> {
-
-}
